aaf4dde2c9dc52a5e2b8ac7dd647b6c3beab8b44
Lucas Povedano Tundo <81191092+tundao05@users.noreply.github.com>
2022-06-20 02:39:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const sideMenu = document.querySelector('aside')
+const menuBtn = document.querySelector('#menu-btn')
+const closeBtn = document.querySelector('#close-btn')
+const themeToggler = document.querySelector('.theme-toggler')
+
+//Mostrar o menu
+menuBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'block'
+})
+
+//Fechar o menu
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none'
+})
+
+//Mudar o tema
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables')
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+
+Orders.forEach(order => {
+  const tr = document.createElement('tr')
+  const trContent = ` 
+                      <td>${order.productName}</td>
+                      <td>${order.productNumber}</td>
+                      <td>${order.paymentStatus}</td>
+                      <td class="${
+                        order.shipping === 'Recusado'
+                          ? 'danger'
+                          : order.shipping === 'Pendente'
+                          ? 'warning'
+                          : order.shipping === 'Recebido'
+                          ? 'success'
+                          : 'primary'
+                      }">${order.shipping}</td>
+                      <td class="primary">Detalhes</td>
+                    `
+  tr.innerHTML = trContent
+  document.querySelector('table tbody').appendChild(tr)
+})
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Vencido',
+    shipping: 'Pendente'
+  },
+  {
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Devolvido',
+    shipping: 'Recusado'
+  },
+  {
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Vencido',
+    shipping: 'Pendente'
+  },
+  {
+    productName: 'Drone com Câmera',
+    productNumber: '96996',
+    paymentStatus: 'Pago',
+    shipping: 'Enviado'
+  },
+  {
+    productName: 'GPS 4K Drone',
+    productNumber: '22821',
+    paymentStatus: 'Pago',
+    shipping: 'Recebido'
+  },
+  {
+    productName: 'DJI Air 2S',
+    productNumber: '81475',
+    paymentStatus: 'Vencido',
+    shipping: 'Pendente'
+  },
+  {
+    productName: 'Lozenge Drone',
+    productNumber: '00482',
+    paymentStatus: 'Pago',
+    shipping: 'Enviado'
+  }
+]
